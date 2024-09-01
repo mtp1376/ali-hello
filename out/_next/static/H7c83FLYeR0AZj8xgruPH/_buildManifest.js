2c218e674f11781e4b1558711854d7de26f73a00
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2024-09-01 14:48:11 +0330

    Deploy static site to GitHub Pages

---
+++ out/_next/static/H7c83FLYeR0AZj8xgruPH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/628765f20b848f76.css","static/chunks/pages/index-5de3650723a8bb1b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
